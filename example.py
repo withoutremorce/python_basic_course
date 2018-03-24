cb234ebffe733fcef0152655c5642edff9b6587c
withoutremorce <withoutremorce@gmail.com>
2018-03-24 14:22:15 +0200

    add example.py

---
+++ example.py
@@ -0,0 +1,31 @@
+int = 1
+float = 1.1
+list = ["dada", "dad1"]
+tuple = ("gaga", 1)
+dict = {0:"1",1:"adad"}
+string = "hahah"
+char = ".d.dd.dk1!!!"
+
+stroka = "hfhfaj"
+
+
+
+print(dict[0])
+
+print (type (int))
+print (type (float))
+print (type (list))
+print (type (tuple))
+print (type (dict))
+print (type (string))
+print (type (char))
+
+
+"""
+начало ввода
+"""
+name = input("Введите имя - ") #ввод имени
+phone_number = input("Введите телефон - ")
+group = input("Введите группу - ")
+
+print(f"<{name}>:<{phone_number}> [<{group}>]")
